ae4d062ede3ac4864aa8c246c76abe22081ec274
LeoAnyu <1115495923@qq.com>
2022-02-17 22:29:33 +0800

    version.202202172228

---
+++ app/src/main/java/com/example/activity/Activity_About.java
@@ -0,0 +1,39 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Activity_About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Button button3 = (Button) findViewById(R.id.activity_about_03);  //访问我们
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_website = new Intent(Intent.ACTION_VIEW);
+                intent_website.setData(Uri.parse("http://www.baidu.com/"));
+                startActivity(intent_website);
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.activity_about_04);  //致电我们
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_website = new Intent(Intent.ACTION_VIEW);
+                intent_website.setData(Uri.parse("tel:8617852839865"));
+                startActivity(intent_website);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activity/Activity_Main.java
@@ -0,0 +1,54 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_layout);
+        Button button = (Button) findViewById(R.id.activity_main_layout_button_manage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Activity_Main.this, "功能尚未实现！", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+
+            case R.id.activity_main_layout_menu_add:
+                Toast.makeText(Activity_Main.this, "添加功能尚未实现！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.activity_main_layout_menu_delete:
+                // Toast.makeText(Activity_Main.this, "删除功能尚未实现！", Toast.LENGTH_SHORT).show();
+                Intent intent_delete = new Intent("activity_delete_layout");
+                intent_delete.addCategory("android.intent.category.DEFAULT");
+                startActivity(intent_delete);
+                break;
+            case R.id.activity_main_layout_menu_about:
+                Intent intent_about = new Intent(Activity_Main.this, Activity_About.class);
+                startActivity(intent_about);
+                break;
+
+            default:
+        }
+        return true;
+    }
+}
